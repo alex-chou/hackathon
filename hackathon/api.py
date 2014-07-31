ed71cdf3d7c678193359476933693b1afa4b7ce4
Swagat Patel <swagat.patel@fivestars.com>
2014-07-31 04:35:35 -0700

    implement CORS in rest API

--- hackathon/api.py
+++ hackathon/api.py
@@ -1,21 +1,56 @@
 from django.contrib.auth.models import User
+from django.http import HttpResponse
+from tastypie import http
 from tastypie.resources import ModelResource
+from tastypie.exceptions import ImmediateHttpResponse
 from core.models import Recreation, Reservation
 
 
-class RecreationResource(ModelResource):
+class CORSResource(ModelResource):
+    """
+        Adds CORS headers to resources that subclass this.
+    """
+    def create_response(self, *args, **kwargs):
+        response = super(CORSResource, self).create_response(*args, **kwargs)
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Headers'] = 'Content-Type'
+        return response
+
+    def method_check(self, request, allowed=None):
+        if allowed is None:
+            allowed = []
+
+        request_method = request.method.lower()
+        allows = ','.join([_.upper() for _ in allowed])
+
+        if request_method == 'options':
+            response = HttpResponse(allows)
+            response['Access-Control-Allow-Origin'] = '*'
+            response['Access-Control-Allow-Headers'] = 'Content-Type'
+            response['Allow'] = allows
+            raise ImmediateHttpResponse(response=response)
+
+        if not request_method in allowed:
+            response = http.HttpMethodNotAllowed(allows)
+            response['Allow'] = allows
+            raise ImmediateHttpResponse(response=response)
+
+        return request_method
+
+
+class RecreationResource(CORSResource):
     class Meta:
         queryset = Recreation.objects.all()
         resource_name = "recreations"
 
 
-class UserResource(ModelResource):
+class UserResource(CORSResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = "users"
 
 
-class ReservationResource(ModelResource):
+class ReservationResource(CORSResource):
     class Meta:
         queryset = Reservation.objects.all()
         resource_name = "reservations"
