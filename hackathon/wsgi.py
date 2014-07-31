4e3fe197aab17b74ecfe639e687edd68d2721c94
Swagat Patel <swagat.patel@fivestars.com>
2014-07-31 02:55:46 -0700

    revert motherf*cing changes

--- hackathon/wsgi.py
+++ hackathon/wsgi.py
@@ -6,10 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-import os
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
+import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackathon.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+from dj_static import Cling
 
 application = Cling(get_wsgi_application())
