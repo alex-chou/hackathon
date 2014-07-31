1fc33cbe016ddecd31b24b9ebe33de60a23d1b09
Swagat Patel <swagat.patel@fivestars.com>
2014-07-31 02:25:46 -0700

    add admin for core models

--- hackathon/core/admin.py
+++ hackathon/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from models import Recreation, Reservation
 
-# Register your models here.
+class RecreationAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "max_duration", "reservation_cooldown"]
+    search_fields = ["name"]
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ["id", "notes", "created_at"]
+    raw_id_fields = ["user"]
+
+
+# register the admins
+admin.site.register(Recreation, RecreationAdmin)
+admin.site.register(Reservation, ReservationAdmin)
+
